3ef866d8165f06858835d47f01767e18acd8a0d4
Fabricio Valdivia <walter.valdivia7685242@gmail.com>
2026-03-18 19:56:54 -0500

    proyecto-cloudcamp-terr-cicd-06

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 resource "aws_s3_bucket" "example" {
-  bucket = "921292479940-walter-valdivia-1"
+  bucket = "921292479940-walter-valdivia"
 
   tags = {
     owner    = "Walter-Valdivia"
     bootcamp = "DevOps"
+    Name     = "walter-valdivia"
   }
 }
